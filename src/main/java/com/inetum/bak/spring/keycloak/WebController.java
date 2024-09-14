f287fbd1cd1fa25202ab2d4e4f9e5a61999c3675
mbak <michal.bak@cumulocity.com>
2024-09-14 14:17:39 +0200

    NotAuthorized resource and pretty response for secured resource

--- src/main/java/com/inetum/bak/spring/keycloak/WebController.java
+++ src/main/java/com/inetum/bak/spring/keycloak/WebController.java
@@ -6,16 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
 
+import static java.lang.String.format;
+
 @Controller
 public class WebController {
 
     @GetMapping(path = "/currentUser")
     public ResponseEntity currentUser(Principal principal) {
-        return ResponseEntity.ok(principal);
+        return ResponseEntity.ok(format("Hello %s from protected resource", principal.getName()));
     }
 
     @GetMapping(path = "/public")
     public ResponseEntity notAuthorizedResource() {
-        return ResponseEntity.ok("Access to public resource.");
+        return ResponseEntity.ok("Public resource.");
     }
 }
